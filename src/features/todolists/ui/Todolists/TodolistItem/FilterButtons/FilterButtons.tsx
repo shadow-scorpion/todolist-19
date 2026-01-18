687e927bbd99e89e50c064f63d8c90b72c17400b
Alex <secret.teamplayer@gmail.com>
2026-01-18 23:24:14 +0200

    Changed local filter data for todolists

--- src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/FilterButtons/FilterButtons.tsx
@@ -1,12 +1,12 @@
 import { useAppDispatch } from "@/common/hooks"
 import { containerSx } from "@/common/styles"
 import {
-  changeTodolistFilterAC,
   type DomainTodolist,
   type FilterValues,
 } from "@/features/todolists/model/todolists-slice"
 import Box from "@mui/material/Box"
 import Button from "@mui/material/Button"
+import { todolistsApi } from "@/features/todolists/api/todolistsApi.ts"
 
 type Props = {
   todolist: DomainTodolist
@@ -18,7 +18,13 @@ export const FilterButtons = ({ todolist }: Props) => {
   const dispatch = useAppDispatch()
 
   const changeFilter = (filter: FilterValues) => {
-    dispatch(changeTodolistFilterAC({ id, filter }))
+    dispatch(
+      todolistsApi.util.updateQueryData('getTodolists', undefined, (todolists) => {
+        const todolist = todolists.find((todolist) => todolist.id === id)
+        if (todolist) {
+          todolist.filter = filter
+        }
+      }))
   }
 
   return (
