7055c82c672ad6a6ee7f436aab17ffd113cd8414
Alex <secret.teamplayer@gmail.com>
2026-01-21 20:31:30 +0200

    Added wrap async func  forfetchBaseQuery

--- src/app/baseApi.ts
+++ src/app/baseApi.ts
@@ -4,14 +4,17 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const baseApi = createApi({
   reducerPath: "todolistsApi",
   tagTypes: ["Todolist", "Task"],
-  baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_BASE_URL,
-    headers: {
-      'API-KEY': import.meta.env.VITE_API_KEY,
-    },
-    prepareHeaders: (headers) => {
-      headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
-    },
-  }),
+  baseQuery: async (arg, api, extraOptions) => {
+    const res = await fetchBaseQuery({
+      baseUrl: import.meta.env.VITE_BASE_URL,
+      headers: {
+        "API-KEY": import.meta.env.VITE_API_KEY,
+      },
+      prepareHeaders: (headers) => {
+        headers.set("Authorization", `Bearer ${localStorage.getItem(AUTH_TOKEN)}`)
+      },
+    })(arg, api, extraOptions)
+    return res
+  },
   endpoints: () => ({}),
 })
